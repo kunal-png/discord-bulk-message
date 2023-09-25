762ac6149893ca94798175470d96a8eaef1e1d92
kunal-png <103259170+kunal-png@users.noreply.github.com>
2023-09-25 13:25:01 +0530

    Update send2discord.py

--- send2discord.py
+++ send2discord.py
@@ -29,6 +29,7 @@ for command in commands:
     print(f"Processing command: {command}")  
     pyperclip.copy(command)  # Copy the command to the clipboard
     pyautogui.hotkey('ctrl', 'v')  # Paste the command
+    time.sleep(2)
     pyautogui.press('enter')  # Press the 'enter' key to send the message
     time.sleep(delay)  # Wait for the specified delay before sending the next command
 
